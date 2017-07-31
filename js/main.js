4d9fe3d7e60e3ec773a764fa7938438c229d19a9
Gilberto Diaz <gdiaz@Gilbertos-MacBook-Pro.local>
2017-07-31 16:03:46 -0500

    loading json done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 {
-  // Reading json data.
-  Chatty.getJsonData = function() {
-    let jsonFile = $.getJSON('./js/chatty.json', function(data) {
-      console.log('Data: ', data);
+  Chatty.getJsonData = function () {
+    $.ajax({
+      url: 'js/chatty.json',
+    }).done(function (data) {
+      Chatty.renderJsonData(data);
     });
   };
+  Chatty.getJsonData();
   // End of Reading json data.
 
   // Render json data in page.
-  Chatty.renderJsonData = function() {
-    let data = Chatty.getJsonData();
-    let
-  }
-  // End of Render json data in page.
+  Chatty.renderJsonData = function (jsonData) {
+    let messageContainer = document.querySelector('.message-container');
+    let items = ''
+    let keys = Object.keys(jsonData)
 
+    keys.forEach(function(item) {
+      items += `<li class="list-group-item justify-content-between">
+                    ${jsonData[item]}
+                    <span class="badge badge-default badge-pill">14</span>
+                </li>`
+    });
+    messageContainer.innerHTML = items;
+  };
+  // End of Render json data in page.
 }
