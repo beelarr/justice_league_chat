04ad6681d38eeac234dc8523cc5a83acc918377d
Gilberto Diaz <gdiaz@Gilbertos-MacBook-Pro.local>
2017-07-31 11:59:52 -0500

    finished basic masseging logic

--- js/message.js
+++ js/message.js
@@ -3,10 +3,34 @@
 
   Chatty.getAllMessages = function() {
     return messages;
-  }
+  };
 
+  Chatty.deleteAllMessages = function() {
+    return messages = {};
+  };
+
+  /**
+   * Inserts a new message into the messages object.
+   *
+   * @param message {string} : new message
+   * @returns {object} : all messages object
+   */
   Chatty.insertNewMessage = function(message) {
-    let index = message.length;
-    messages[index+1] = message;
-  }
+    let indexes = Object.keys(messages);
+    if (indexes.length === 0) {
+      messages[0] = message;
+      return messages;
+    } else {
+      let reverseIndexes = indexes.reverse()
+      let lastIndex = parseInt(reverseIndexes[0])
+      messages[lastIndex+1] = message;
+      return messages;
+    }
+  };
+
+  Chatty.deleteSingleMessage = function(id) {
+    return delete messages[id];
+    return messages;
+  };
+
 }
