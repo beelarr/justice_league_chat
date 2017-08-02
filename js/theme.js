7eba5ae2194b270ec8bffa00e69b7b8badddc09f
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-02 11:45:22 -0500

    Added modal and logic in theme.js for modal

--- js/theme.js
+++ js/theme.js
@@ -28,5 +28,34 @@ window.onload = start;
 
 
 
+$(document).ready(function () {
+    $('#register').modal('show');
+
+});
+
+
+function changeStyleSheet(theme) {
+    document.getElementById("pagetheme").setAttribute("href", theme);
+}
+
+function start() {
+    let DefaultTheme = document.getElementById("DefaultThemeModal");
+    let DarkKnight = document.getElementById("DarkKnightModal");
+
+    DefaultTheme.onclick = function () {
+        changeStyleSheet("./css/main.css")
+    };
+
+    DarkKnight.onclick = function () {
+        changeStyleSheet("./css/test-css.css");
+    };
+}
+
+window.onload = start;
+
+
+
+
+
 
 
